17c455ce79dcc4d706370919a53696078d94675e
dracozero7 <dracozero7@outlook.com>
2021-10-26 13:38:07 -0500

    Fixing Provider

--- main.tf
+++ main.tf
@@ -6,10 +6,18 @@ terraform {
   }
 }
 
-provider "oci" {
-  region              = "us-ashburn-1"
+/*provider "oci" {
+  region              = "us-ashburn-2"
   auth                = "SecurityToken"
   config_file_profile = "OCITFTEST"
+}*/
+
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
 }
 
 resource "oci_core_virtual_network" "vcn" {
--- variables.tf
+++ variables.tf
@@ -6,8 +6,23 @@ variable "tenancy_ocid" {
   default = "ocid1.tenancy.oc1..aaaaaaaasebb2h3a564nvf6ekja53uhy3rto4o3imikhgeuni2yyohsukesq"
 }
 
+variable "user_ocid" {
+  default = "ocid1.user.oc1..aaaaaaaagpnf5ea5azxgo3xzu7uagylgys6ufupaaiasndxlojlwfkuxiwva"
+}
+
+variable "fingerprint" {
+  default = "e2:ec:e9:7e:74:d3:9b:5a:1a:e7:d4:6b:1d:cc:db:c5"
+}
+
+variable "private_key_path" {
+  default = "/home/opc/.oci"
+}
 variable "AD" {
   default = "2"
+}
+
+variable "region" {
+  default = "us-ashburn-2"
 }
 
 variable "vcn_cidr" {
